b345030c89fb54a06bb9148d68fb80665ee62364
Mark <mlang41110@yahoo.com>
2016-11-22 15:19:18 -0500

    added comments

--- ReactInit.bat
+++ ReactInit.bat
@@ -1,23 +1,46 @@
 echo off
 cls
+
+:: Enable command extensions so that the "if not defined" syntax becomes valid
+::
 setlocal enableextensions
 
+:: Store the first parameter in a variable, if a parameter was not defined, set the variable equal to "newApp"
+::
 set folderName=%1
 if not defined folderName set foldername=newApp
 
+
+:: If the folder doesn't exist, make it
+::
 if not exist %folderName% (
 	md %folderName%
 )
+
 cd %folderName%
 
 
+:: Initialize NPM and use it download React, Babel, and Webpack
+::
 call npm init -y
 call npm i -S react react-dom
 call npm i -D babel-core babel-loader babel-preset-react
 call npm i -D webpack webpack-dev-server html-webpack-plugin
 
+
+:: Create the .babelrc configuration file
+:: This allows Babel to transform React into normal JS
+::
 echo {presets: ['react']} > .babelrc
 
+
+
+
+:: Create the webpack.config.js file
+:: This specifies which files to transform, how to transform them, and where to put the transformed files
+:: The transformed files will be index.html and transformed.js, these will both be in the newly created "build" folder
+:: Webpack will inject a script tag linking to transformed.js in the body of index.html
+::
 echo var HTMLWebpackPlugin = require('html-webpack-plugin'); > webpack.config.js
 echo var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({ >> webpack.config.js
 echo 	template: __dirname + '/app/index.html', >> webpack.config.js
@@ -43,10 +66,27 @@ echo 	}, >> webpack.config.js
 echo 	plugins: [HTMLWebpackPluginConfig] >> webpack.config.js
 echo }; >> webpack.config.js
 
+
+
+
+:: Modify the package.json file
+:: This function will iterate through all the lines in package.json and insert custom scripts to be run from NPM
+:: 		Does this by copying line by line from package.json into temp.txt
+:: The for loop is searching for the string "  "scripts"", full colons are the delimeters
+:: 		When it finds the string, two custom scripts will be inserted into temp.txt
+:: Afterwards, the for loop continues copying lines from package.json to temp.txt
+:: Finally, package.json is overwritten with the contents of temp.txt and temp.txt is deleted
+::
+:: The custom scripts are:
+::		npm run build - This will transform the React into regular JS and output the transformed files in the "build" folder
+::		npm run start - This will run the Webpack server, so that the user can view the React app at http://localhost:8080
+::
 for /f "tokens=1,* delims=:" %%G in (package.json) do (
 	if "%%H"=="" (
+		:: This check is necessary because if there is no semicolon in the line, we only want to echo the first token
 		echo %%G >> temp.txt
 	) else (
+		:: Otherwise, we can echo the first token, then a semicolon, then the second token, which is the rest of the line
 		echo %%G:%%H >> temp.txt
 	)
 	
@@ -56,12 +96,18 @@ for /f "tokens=1,* delims=:" %%G in (package.json) do (
 	)
 )
 
+
+:: Overwrite package.json with the contents of temp.txt, then delete temp.txt
+::
 type temp.txt > package.json
 del /q temp.txt
 
 md app\components
 cd app
 
+
+:: Create a basic index.html file
+::
 echo ^<!DOCTYPE html^> > index.html
 echo ^<html^> >> index.html
 echo 	^<head^> >> index.html
@@ -73,6 +119,9 @@ echo 		^<div id = "app"^>^</div^>  >> index.html
 echo 	^</body^>                     >> index.html
 echo ^</html^> >> index.html
 
+
+::Create a basic index.js file to render the React App component
+::
 echo var React = require('react'); > index.js
 echo var ReactDOM = require('react-dom'); >> index.js
 echo var App = require('./components/App'); >> index.js
@@ -81,6 +130,9 @@ echo ReactDOM.render(^<App /^>, document.getElementById('app')); >> index.js
 
 cd components
 
+
+:: Create a basic App.js React component to render a "Hello World" h1
+::
 echo var React = require('react'); > App.js
 echo: >> App.js
 echo var App = React.createClass({ >> App.js
@@ -91,7 +143,23 @@ echo }); >> App.js
 echo: >> App.js
 echo module.exports = App; >> App.js
 
+
+:: Transform the React files into regular JS
+::
 call npm run build
+
+
+echo:
+echo:
+echo:
+echo:
+echo React app successfully built
+echo Navigate to the folder and call npm run start to run the Webpack server
+echo The app can be viewed at http://localhost:8080
+echo:
+echo:
+
+
 
 
 
